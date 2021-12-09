25a4a337bc8ee64efa4c5a469b1b7387b09f0aca
Siny <sinydaniel93@gmail.com>
2021-12-09 21:03:17 +0530

    roboshop project

--- roboshop.sh/roboshop.sh
+++ roboshop.sh/roboshop.sh
@@ -5,3 +5,17 @@ if [ "${USER_UID}" -ne 0 ]; then
   exit
 fi
 
+COMPONENT=$1
+
+if [ -z "$COMPONENT" ]; then
+  echo -e "\e[1;31mcomponent input missing\e[0m"
+  exit
+fi
+
+if [ ! -e components/${COMPONENT}.sh ]; then
+  echo -e "\e[1;31mgiven script doesnot exist\e[0m"
+  exit
+fi
+
+bash components/${COMPONENT}.sh
+
