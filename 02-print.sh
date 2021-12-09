3c581a39ec5346c2125a00ba8617d2f61a8e39bf
Siny <sinydaniel93@gmail.com>
2021-12-09 11:56:04 +0530

    print in color codes

--- 02-print.sh
+++ 02-print.sh
@@ -2,3 +2,13 @@
 
 echo hello
 echo hello world
+
+## print in colors
+
+echo -e "\e[31mPrint in red color\e[0m"
+echo -e "\e[32mPrint in green color\e[0m"
+echo -e "\e[33mPrint in yellow color\e[0m"
+echo -e "\e[34mPrint in blue color\e[0m"
+echo -e "\e[35mPrint in magenta color\e[0m"
+echo -e "\e[36mPrint in cyan color\e[0m"
+
