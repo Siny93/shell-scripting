6ff20314dd643dd17ed425b6cd428b927bcc118c
Siny <sinydaniel93@gmail.com>
2021-12-09 21:02:11 +0530

    roboshop project

--- roboshop.sh/roboshop.sh
+++ roboshop.sh/roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USER_UID=$(id -u)
-if [ "${USER_ID}" -ne 0 ]; then
+if [ "${USER_UID}" -ne 0 ]; then
   echo -e "\e[1;31mYou should be root user to perform this script\e[0m"
   exit
 fi
