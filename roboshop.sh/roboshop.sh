c2fce1bfbb1f811c4f10877a20be93f029ba031c
Siny <sinydaniel93@gmail.com>
2021-12-09 20:46:16 +0530

    roboshop project

---
+++ roboshop.sh/roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USER_UID=$(id -u)
+if [ ${USER-ID} -ne 0 ]; then
+  echo -e "\e[1;31mYou should be root user to perform this script\e[0m"
+  exit
+fi
+
+COMPONENT=$1
+
+if [ -z "$COMPONENT" ]; then
+  echo -e "\e[1;31mcomponent input missing\e[0m"
+  exit
+fi
+
+if [ ! -e components/${COMPONENT}.sh ]; then
+  echo -e "\e[1;31mgiven script doesnot exist\e[0m"
+  exit
+fi
+
+bash components/${COMPONENTS}.sh
