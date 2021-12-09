0182d12441901445f7b77d74f6cdca35117067bd
Siny <sinydaniel93@gmail.com>
2021-12-09 12:04:08 +0530

    print in color codes and bold

--- 02-print.sh
+++ 02-print.sh
@@ -12,3 +12,5 @@ echo -e "\e[34mPrint in blue color\e[0m"
 echo -e "\e[35mPrint in magenta color\e[0m"
 echo -e "\e[36mPrint in cyan color\e[0m"
 
+echo -e "\e[32mnormal green\e[1;32mBold green[0m"
+
