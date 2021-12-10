704fb669b6078300d6c5e3ab00474459bc39172c
Siny <sinydaniel93@gmail.com>
2021-12-10 20:15:58 +0530

    declaring functions updated

--- functionsnew.sh
+++ functionsnew.sh
@@ -16,9 +16,17 @@ sample1()
 sample2(){
   echo first argument = $1
   echo number of arguments = $#
+  a = 300
+  echo a in function = ${a}
+  b = 400
 }
 
-sample
-sample1
+## main pgm
+
+
+a = 100
+
 sample2 123 xyz
+
+echo value of b from main = ${b}
 
