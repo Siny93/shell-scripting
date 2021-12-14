324170736da4ada2b6387c925fc5c4fca3fd8774
Siny <sinydaniel93@gmail.com>
2021-12-14 22:21:53 +0530

    catalogue code

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
 echo catalogue setup
+
+source components/common.sh
+yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
+STAT_CHECK $? "Install Nodejs"
+
+useradd roboshop &>>${LOG_FILE}
+STAT_CHECK $? "Add roboshop user"
+
+DOWNLOAD catalogue
