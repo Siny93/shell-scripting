51e0606ad615ab9c48ec5bfd5c4f4320e892580b
Siny <sinydaniel93@gmail.com>
2021-12-14 12:22:25 +0530

    added mongodb redis rabbitmq and mysql steps

--- roboshop/components/databases.sh
+++ roboshop/components/databases.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#
+source components/common.sh
+
+
 #echo -e "  --------<<<<<<<<< MongoDB setup >>>>>>>----------"
 #
 #
-#source components/common.sh
+#
 #curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
 #STAT_CHECK $? "Download mongodb repo"
 #
