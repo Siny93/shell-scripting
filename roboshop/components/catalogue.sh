b4ab48e3c2f4b53d4cc93873e36f89a004b61bea
Siny <sinydaniel93@gmail.com>
2021-12-14 23:07:52 +0530

    catalogue full code

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -19,7 +19,14 @@ rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cp -r /t
  STAT_CHECK $? "copy catalogue content"
 
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>${LOG_FILE}
-STAT_CHECK $? "copy catalogue content"
+STAT_CHECK $? "install nodejs dependencies"
 
 chown roboshop:roboshop -R /home/roboshop
+
+
+sed -i -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
+STAT_CHECK $? "update systemd config file"
+
+systemctl daemon-reload &>>${LOG_FILE} && systemctl start catalogue &>>${LOG_FILE} && systemctl enable catalogue &>>${LOG_FILE}
+STAT_CHECK $? "start catalogue service"
 
